ead3a35bf26af8128e471040362985a8db5ce82f
shalfbea <shalfbea@student.21-school.ru>
2022-03-18 21:19:05 +0300

    working halfly

--- src/client.c
+++ src/client.c
@@ -6,17 +6,89 @@
 /*   By: shalfbea <shalfbea@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 12:00:18 by shalfbea          #+#    #+#             */
-/*   Updated: 2022/02/08 18:05:04 by shalfbea         ###   ########.fr       */
+/*   Updated: 2022/03/18 19:44:00 by shalfbea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int		send_zero(int server)
+{
+	static int	i;
+
+	while (++i < 9)
+	{
+		kill(server, SIGUSR1);
+		return (1);
+	}
+	return (0);
+}
+
+char	bit_sender(int orig_serverid, char	*orig_msg)
+{
+	static int	serverid;
+	static char	*msg;
+	static int	bits;
+
+	if (orig_msg)
+		msg = ft_strdup(orig_msg);
+	if (orig_serverid)
+		serverid = orig_serverid;
+	if (msg[bits / 8])
+	{
+		if (msg[bits / 8] & (0x80 >> (bits % 8)))
+		{
+			if (kill(serverid, SIGUSR2) == -1)
+				exit(0);
+		}
+		else
+		{
+			if (kill(serverid, SIGUSR1) == -1)
+			exit(0);
+		}
+		bits++;
+		return (0);
+	}
+	//if(msg)
+	//	free(msg);
+	if (send_zero(serverid))
+		return (0);
+	return (1);
+}
+
+void	signal_handler_client(int sig, siginfo_t *info, void *smth)
+{
+	(void) info;
+	(void) smth;
+	if (sig == SIGUSR1)
+	{
+		if (bit_sender(0, 0))
+			exit(0);
+	}
+	else if (sig == SIGUSR2)
+	{
+		exit(0);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		kill(ft_atoi(argv[1]), )
-	}
+	t_sigacton	sigs;
+	sigset_t	set;
+
+	if (argc != 3)
+		exit(0);
+	memset(&sigs, 0, sizeof(sigs));
+	sigs.sa_sigaction = signal_handler_client;
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigaddset(&set, SIGUSR2);
+	sigs.sa_mask = set;
+	sigaction(SIGUSR1, &sigs, 0);
+	sigaction(SIGUSR2, &sigs, 0);
+	bit_sender(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		//usleep(1);
+		pause();
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,37 +6,62 @@
 /*   By: shalfbea <shalfbea@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 12:00:41 by shalfbea          #+#    #+#             */
-/*   Updated: 2022/02/08 17:59:49 by shalfbea         ###   ########.fr       */
+/*   Updated: 2022/03/18 19:41:34 by shalfbea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int sig)
+void	signal_handler(int sig, siginfo_t *info, void *smth)
 {
-	ft_putstr_fd("received smth", 1);
-	ft_putnbr_fd(sig, 1);
-	exit(0);
+	static char	cur;
+	static int	i;
+	static int client;
+	//static char	end;
+	//ft_putstr_fd("received sig:", 1);
+	//ft_putnbr_fd(sig, 1);
+	//ft_putstr_fd("\n", 1);
+
+	//ft_putnbr_fd(client, 0);
+	(void) info;
+	(void) smth;
+	if (info->si_pid)
+		client = info->si_pid;
+	if (i == 0)
+		cur = 0xFF;
+	if (sig == SIGUSR1)
+		cur^= 0x80 >> i;
+	else if (sig == SIGUSR2)
+		cur|= 0x80 >> i;
+	i++;
+	//cur = 'c'q;
+	if (i == 8)
+	{
+		if (cur)
+			write(0, &cur, 1);
+		i = 0;
+		cur = 0xFF;
+	}
+	kill(client, SIGUSR1);
+	//usleep(3);
 }
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	t_sigacton sigs;
+	t_sigacton	sigs;
+	sigset_t	set;
 
-	(void) argc;
-	(void) argv;
 	memset(&sigs, 0, sizeof(sigs));
-	sigs.sa_handler = signal_handler;
-	sigset_t   set;
+	sigs.sa_sigaction = signal_handler;
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
 	sigaddset(&set, SIGUSR2);
 	sigs.sa_mask = set;
 	sigaction(SIGUSR1, &sigs, 0);
 	sigaction(SIGUSR2, &sigs, 0);
-	ft_putnbr_fd(getpid(), 1);
 
+	ft_putnbr_fd(getpid(), 1);
 	while (1)
-		usleep(1);
+		pause();
 	return (0);
 }
